7591be424027cb71496d54ba2b173a7ed6a34f6f
ngaruko <bede.ngaruko@gmail.com>
2021-11-08 17:03:03 +1300

    new language specs

--- tests/page-objects/reports/reports.wdio.page.js
+++ tests/page-objects/reports/reports.wdio.page.js
@@ -7,7 +7,7 @@ const submitterPlace = () => $('.position a');
 const submitterPhone = () => $('.sender .phone');
 const submitterName = () => $('.sender .name');
 const firstReport = () => $(`${reportListID} li:first-child`);
-const reportList = () => $(`${reportListID}`);
+const reportList = () => $(reportListID);
 const submitReportButton = () => $('.action-container .general-actions:not(.ng-hide) .fa-plus');
 const formActionsLink = (formId) => {
   return $(`.action-container .general-actions .dropup.open .dropdown-menu li a[href="#/reports/add/${formId}"]`);
