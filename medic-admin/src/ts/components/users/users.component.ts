de7e457d833b07df7a7d9395e87b013360c52171
ngaruko <bede.ngaruko@gmail.com>
2021-12-29 17:47:50 +1300

    added user template

--- medic-admin/src/ts/components/users/users.component.ts
+++ medic-admin/src/ts/components/users/users.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-users',
+  selector: 'mm-users',
   templateUrl: './users.component.html',
   styles: [
   ]
 })
 export class UsersComponent implements OnInit {
+users = [
+  {
+    name: "Me",
+    fullname: "james bond",
+    roles : [0, 1, 2],
+    phone : '097465'
+}]
+
+roles =[{
+  name : 'regular user'
+}]
 
   constructor() { }
 
@@ -14,3 +25,4 @@ export class UsersComponent implements OnInit {
   }
 
 }
+
