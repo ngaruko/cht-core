71083d66e075617f061bf62de49cdbb5706013ed
ngaruko <bede.ngaruko@gmail.com>
2021-12-29 20:23:39 +1300

    adding code to app component

--- medic-admin/src/ts/app.component.ts
+++ medic-admin/src/ts/app.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+//import { SessionService } from '@mm-services/session.service';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'medic-admin';
+export class AppComponent implements OnInit {
+  //private globalActions;
+
+  title: string;
+  webAppUrl: string;
+
+  constructor (
+    //private sessionService:SessionService,
+    // private resourceIconsService:ResourceIconsService,
+    // private locationService:LocationService,
+    // private formatDateService:FormatDateService,
+  ) {
+    //this.globalActions = new GlobalActions(store);
+    //this.analyticsActions = new AnalyticsActions(store);
+
+    this.webAppUrl = 'http://localhost:5988/messages';
+    this.title ='Admon Web app';
+  }
+
+
+
+
+
+  ngOnInit(): void {
+    //this.globalActions.setIsAdmin(this.sessionService.isAdmin())
+    //this.setAppTitle();
+  }
+
+  logout() {
+    //this.sessionService.logout();
+   console.log('I am out...')
+  }
 }
--- medic-admin/src/ts/components/header/header.component.ts
+++ medic-admin/src/ts/components/header/header.component.ts
@@ -1,19 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { combineLatest, Subscription } from 'rxjs';
+// import { SettingsService } from '@mm-services/settings.service';
+// import { HeaderTabsService } from '@mm-services/header-tabs.service';
+// import { AuthService } from '@mm-services/auth.service';
+//import { SessionService } from '@mm-services/session.service';
+// import { GlobalActions } from '@mm-actions/global';
+// import { Selectors } from '@mm-selectors/index';
+
+
 
 @Component({
   selector: 'mm-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
-  webAppUrl = 'http://localhost:5984';
+export class HeaderComponent implements OnInit, OnDestroy {
+  private subscription: Subscription = new Subscription();
 
-  constructor() { }
+  @Input() webAppUrl: any;
+
+  showPrivacyPolicy = false;
+  replicationStatus: unknown;
+  currentTab: unknown;
+  unreadCount = {};
+  permittedTabs = [];
+  title = 'Medic Amin App';
+
+  private globalActions;
+
+  constructor(
+
+    // private settingsService: SettingsService,
+    // private headerTabsService: HeaderTabsService,
+    // private authService: AuthService,
+     //private sessionService: SessionService
+  ) {
+    //this.globalActions = new GlobalActions(store);
+  }
 
   ngOnInit(): void {
+    // this.subscribeToStore();
   }
 
-  logout() {
-    console.log('logging out!')// sessionservice.logout()
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
+
+logout(){
+  //this.sessionService.logout();
+  alert('Logging out ');
+}
 
 }
+
