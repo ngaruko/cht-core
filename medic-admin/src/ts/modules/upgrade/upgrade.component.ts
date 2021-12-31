55bc533400e79f76b221f1f70d8e20ff76032de0
ngaruko <bede.ngaruko@gmail.com>
2022-01-01 11:23:49 +1300

    retouch upgrade components

--- medic-admin/src/ts/modules/upgrade/upgrade.component.ts
+++ medic-admin/src/ts/modules/upgrade/upgrade.component.ts
@@ -11,6 +11,13 @@ export class UpgradeComponent implements OnInit {
   currentDeploy = false;
 loading = false;
 instance = {upgrade: {no_horti: {detail:'090'}}}
+versions= {
+  branches:['dev', 'prod', 'test'],
+  releases:['dev', 'prod', 'test'],
+  betas:['dev', 'prod', 'test']
+}
+deployDoc = {_dleteted:false, action: 'retry', staging_complete:true}
+error = 'some error'
 
   constructor() { }
 
